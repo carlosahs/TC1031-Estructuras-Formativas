82e32a11b485a52a3a4d1817812ccbd71a9421f4
Carlos Hurtado <tacahs12@gmail.com>
2021-08-19 12:57:11 -0500

    Declare sum functions

--- Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.h
@@ -1,3 +1,11 @@
 #ifndef FUNCIONES
 #define FUNCIONES
 #endif
+
+class Funciones
+{
+    public:
+        int sumaDirecta(int num);
+        int sumaIterativa(int num);
+        int sumaRecursiva(int num);
+};
