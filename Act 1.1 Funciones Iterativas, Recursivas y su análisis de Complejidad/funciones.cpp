3930ee381f4a699059a4ee784d26b705fa031d9c
Carlos Hurtado <tacahs12@gmail.com>
2021-08-19 12:57:33 -0500

    Define sum functions

---
+++ Act 1.1 Funciones Iterativas, Recursivas y su análisis de Complejidad/funciones.cpp
@@ -0,0 +1,30 @@
+#include "funciones.h"
+
+int Funciones::sumaDirecta(int num)
+{
+    return (num * num + num) / 2;
+}
+
+int Funciones::sumaIterativa(int num)
+{
+    int sum = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum += i;
+    }
+
+    return sum;
+}
+
+int Funciones::sumaRecursiva(int num)
+{
+    if (num == 1)
+    {
+        return num;
+    }
+    else if (num == 0)
+    {
+        return num;
+    }
+    return num + sumaRecursiva(num - 1);
+}
