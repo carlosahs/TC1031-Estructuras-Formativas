be015599b144351f0d803d3f5cdece782b043b2e
carlosahs <tacahs12@gmail.com>
2021-09-08 17:37:22 -0500

    Modify method argument to be a reference

--- Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/sorts.h
+++ Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/sorts.h
@@ -10,10 +10,10 @@ class Sorts
         void ordenaSeleccion(std::vector<T>);
         void ordenaBurbuja(std::vector<T>);
         void ordenaMerge(std::vector<T>);
-        T busqSecuencial(std::vector<T>, T);
-        T busqBinaria(std::vector<T>, T);
+        int busqSecuencial(std::vector<T>, T);
+        int busqBinaria(std::vector<T>, T);
     private:
-        void swap(std::vector<T>, int, int);
+        void swap(std::vector<T>&, int, int);
 };
 
 #endif
