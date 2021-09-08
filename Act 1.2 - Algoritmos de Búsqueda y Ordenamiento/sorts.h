b2d2f10f2773833b7d67f0749142ddcd029a2803
carlosahs <tacahs12@gmail.com>
2021-09-08 16:58:23 -0500

    Define header class

---
+++ Act 1.2 - Algoritmos de Búsqueda y Ordenamiento/sorts.h
@@ -0,0 +1,19 @@
+#ifndef SORTS_H
+#define SORTS_H
+
+#include <vector>
+
+template<typename T>
+class Sorts
+{
+    public:
+        void ordenaSeleccion(std::vector<T>);
+        void ordenaBurbuja(std::vector<T>);
+        void ordenaMerge(std::vector<T>);
+        void busqSecuencial(std::vector<T>);
+        void busqBinaria(std::vector<T>);
+    private:
+        void swap(std::vector<T>, int, int);
+};
+
+#endif
